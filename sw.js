8ee054182172e533fc9d5dd75bc4bef5e66d759e
TemperatureDashboard <action@github.com>
2026-03-02 08:10:58 +0900

    fix: SW Cache v9に更新・CSSバージョン上げてスマホキャッシュ強制更新

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for 外気温モニター PWA
 // Version 4 - Push Notification Support
-const CACHE_NAME = 'temp-monitor-v8';
+const CACHE_NAME = 'temp-monitor-v9';
 const urlsToCache = [
     './',
     './index.html'
