eff3e35e4a75b77ffdb81974ccbd47500bea2785
TemperatureDashboard <action@github.com>
2026-08-01 15:17:31 +0900

    スマホの温度背景を自然に調整

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for 外気温モニター PWA
 // Version 4 - Push Notification Support
-const CACHE_NAME = 'temp-monitor-v10';
+const CACHE_NAME = 'temp-monitor-v11';
 const urlsToCache = [
     './',
     './index.html'
